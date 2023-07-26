c293758112231ba7d9ff6e6de2fb1d95d9372d4a
Adewale Eniola <104868415+3ni0lA@users.noreply.github.com>
2023-07-26 20:34:59 +0100

    Update whileloop.sh

--- whileloop.sh
+++ whileloop.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-counter=0 #the value is true hence why its going to continue looping until stopped using CTRL C#
+counter=0 
+#the value is true hence why its going to continue looping until stopped using CTRL C#
 
-while [ $counter -lt 5 ] #-lt means lesser than#
+while [ $counter -lt 5 ] #-lt means lesser than this iteration will become false and loop will terminate#
 do
   echo "Looping......"
   echo "Value of counter is $counter. "
+  counter=$(($counter + 1)) #
+  sleep 1
 done
 
-
+echo "Out of loop. "
 
